b503df527afa8c5c14af468d5806fce95876d9c6
Gabriel Peplinski <gabrielspeplinski2301@gmail.com>
2020-10-26 21:14:32 -0300

    26-10-2020-- Module Pattern+use strict+checkbox

--- assets/resources/scripts/main-page-script.js
+++ assets/resources/scripts/main-page-script.js
@@ -1,59 +1,81 @@
-(() => {
-    window.alert('✈Bem-vindo ao Bonfim Viagens✈');
-})();
-
-let nome;
-window.addEventListener('onload', getNome());
-
-function getNome() {
-    nome = window.prompt('Qual seu nome?');
-    nome = nome.toUpperCase();
-    confirmacao();
-   function confirmacao(){
-        var answer = window.confirm(`Olá, tudo bem!Então seu nome é ${nome} certo?`);
-   }
-}
-
-document.querySelector('#go-down-button').addEventListener('click', function() {
-        window.scrollTo(0, 630);
-});
-
-//Beneficios - Button
-//usar var inverted =
-benefits1 = ['Fuga da rotina',
-            'Maior criatividade',
-            'Maior confiança e autoestima',
-            'Maior adaptação a mudanças',
-            'Aumento da capacidade cognitiva',
-            'Melhora a função cardíaca',
-            'Aumento da habilidade social',
-            'Criação de boas lembranças',
-            'Vai fazer você se divertir!'
-];
-
-benefits2 = [
-            'Fuga da rotina',
-            'Maior criatividade',
-            'Maior confiança e autoestima',
-            'Maior adaptação a mudanças',
-            'Aumento da capacidade cognitiva',
-            'Melhora a função cardíaca',
-            'Aumento da habilidade social',
-            'Criação de boas lembranças',
-            'Vai fazer você se divertir!'
-];
-
-let i = 0;
-let benefits = benefits1.concat(benefits2).reverse();
-setInterval(function() {
-    function plusOne() {
-    var location = document.getElementById('benefits-space');
-    location.innerHTML = benefits[i];
-    i++;
+(function(){
+    'use strict';
+    (() => {
+        window.alert('✈Bem-vindo ao Bonfim Viagens✈');
+    })();
+    
+    let nome;
+    window.addEventListener('onload', getNome());
+    
+    function getNome() {
+        nome = window.prompt('Qual seu nome?');
+        nome = nome.toUpperCase();
+        confirmacao();
+       function confirmacao(){
+           var answer = window.confirm(`Olá, tudo bem!Então seu nome é ${nome} certo?`);
+       }
     }
-    plusOne();
-}, 3000);
+    
+    document.querySelector('#go-down-button').addEventListener('click', function() {
+            window.scrollTo(0, 630);
+    });
+    
+    //Beneficios - Button
+    //usar var inverted =
+    var benefits1 = ['Fuga da rotina',
+                'Maior criatividade',
+                'Maior confiança e autoestima',
+                'Maior adaptação a mudanças',
+                'Aumento da capacidade cognitiva',
+                'Melhora a função cardíaca',
+                'Aumento da habilidade social',
+                'Criação de boas lembranças',
+                'Vai fazer você se divertir!'
+    ];
+    
+    var benefits2 = [
+                'Fuga da rotina',
+                'Maior criatividade',
+                'Maior confiança e autoestima',
+                'Maior adaptação a mudanças',
+                'Aumento da capacidade cognitiva',
+                'Melhora a função cardíaca',
+                'Aumento da habilidade social',
+                'Criação de boas lembranças',
+                'Vai fazer você se divertir!'
+    ];
+    
+    let i = 0;
+    let benefits = benefits1.concat(benefits2).reverse();
+    setInterval(function() {
+        function plusOne() {
+        var location = document.getElementById('benefits-space');
+        location.innerHTML = benefits[i];
+        i++;
+        if(i > benefits.length){
+            location.innerHTML = "Viajar é Bom!";
+        }
+        }
+        plusOne();
+    }, 3000);
+    
+    document.querySelector('.rateUsStars').addEventListener('submit', function(e){
+        e.preventDefault();
+    });
+    
+}());
 
 function getCidade() {
     window.alert('Tenho uma ótima notícia!Temos uma agência em sua cidade!😃');
+}
+
+function validaCheckboxes(){
+    let checkboxes = document.getElementsByName('stars');
+    for(var i in checkboxes){
+        if(checkboxes[i].checked){
+            isChecked = true;
+            alert('Avaliação enviada!');
+            return true;
+        }
+    }
 }
--- assets/resources/scripts/trade-travels.js
+++ assets/resources/scripts/trade-travels.js
@@ -9,8 +9,8 @@ class Usuario {
 
 class Cliente extends Usuario {
     constructor(nome, email, cpf, cep, telefone) {
-        super(nome, email, cpf, cep );
-        if(checkNumberFunction() === true){
+        super(nome, email, cpf, cep);
+        if (checkNumberFunction() === true) {
             this.telefone = telefone;
         }
     }
@@ -23,143 +23,247 @@ class Admin extends Usuario {
     }
 }
 
-let Travel = function(destino, dias) {
+let Travel = function (destino, dias) {
     this.destino = destino;
     this.dias = dias;
+};
+
+window.onload = function () {
+    let cpfInput = document.querySelector('#cpfInput');
+    cpfInput.addEventListener('blur', function () {
+        if (this.validity.invalid) {
+            this.setCustomValidity('AVISO: Preencha o CPF da maneira correta!');
+        }
+    });
+    cpfInput.addEventListener('blur', function () {
+        if (this.validity.valid) {
+            this.setCustomValidity('Preenchido corretamente!');
+        }
+    });
 };
 
 // Criação de Usuário
 let nomeFromForm;
 let emailFromForm;
 var users = [];
-document.querySelector('#create-login-button').addEventListener('click', function () {
-    let getUserName = document.getElementsByTagName('input')[0];
-    nomeFromForm = getUserName.value;
-    nomeSub = nomeFromForm.substring(0, 1);
-    nomeSub2 = nomeFromForm.substring(1,nomeFromForm.length);
-    nomeSub = nomeSub.toUpperCase();
-    nomeFull = nomeSub + nomeSub2;
-    let getUserEmail = document.getElementsByName('form2')[0];
-    emailFromForm = getUserEmail.value;
-    let cpf = document.forms[0].cpfInput.value;
-    let cep = document.forms[0].cepInput.value;
-    if((checkCEP === true) && (checkCPF === true)){
+document
+    .querySelector('#formCreateUser')
+    .addEventListener('submit', function (e) {
+        let getUserName = document.getElementsByTagName('input')[0];
+        nomeFromForm = getUserName.value;
+        let nomeSub = nomeFromForm.substring(0, 1);
+        let nomeSub2 = nomeFromForm.substring(1, nomeFromForm.length);
+        nomeSub = nomeSub.toUpperCase();
+        let nomeFull = nomeSub + nomeSub2;
+        let getUserEmail = document.getElementsByName('form2')[0];
+        emailFromForm = getUserEmail.value;
+        let cpf = document
+            .forms[0]
+            .cpfInput
+            .value;
+        console.log(cpf);
+        let cep = document
+            .forms[0]
+            .cepInput
+            .value;
+        console.log(cep);
         let user = new Cliente(nomeFull, emailFromForm.toLowerCase(), cpf, cep);
         users.push(user);
         console.log(users[0]);
         alert('Usuário criado com sucesso!');
-    }else{
-        alert('AVISO: Dados inválidos!');
-    }
-});
-
-let checkCPF = false;
-let checkCEP = false;
-window.onload = function(){
-    let cpfInputValue = document.querySelector('#cpfInput').value;
-    cpfInputValue.addEventListener('blur', function(){
-        //FIX
-        let pattern = new RegExp('\d{3}\.?\d{3}\.?\d{3}-?\d{2}');
-        if(pattern.test(cpfInputValue) === true){
-            console.log("verificação cpf certo!");
-            checkCPF = true;
-        }else{
-            console.log("verificação cpf errado");
-            checkCPF = false;
-        }
+        e.preventDefault();
     });
-
-    let cepInputValue = document.querySelector('#cepInput').value;
-    cepInput.addEventListener('blur', function(){
-        let pattern = new RegExp('\d{5}-\d{3}');
-        if(pattern.test(cpfInputValue) === true){
-            console.log("verificação cep certo!");
-            checkCEP = true;
-        }else{
-            console.log("verificação cep errado");
-            checkCEP = false;
-        }
-    });
-};
 
 let formNome = document.querySelector('#nome');
-formNome.addEventListener('focus', function() {
-    document.getElementById('nome').style.backgroundColor = "#EEAD0E";  
+formNome.addEventListener('focus', function () {
+    document
+        .getElementById('nome')
+        .style
+        .backgroundColor = "#EEAD0E";
 });
 
-formNome.addEventListener('blur',function() {
-    document.getElementById("nome").style.backgroundColor = "#CFCFCF";
+formNome.addEventListener('blur', function () {
+    document
+        .getElementById("nome")
+        .style
+        .backgroundColor = "#CFCFCF";
 });
 
 let formEmail = document.querySelector('#email');
-formEmail.addEventListener('focus',function() {
-    document.getElementById('email').style.backgroundColor = "#EEAD0E";
+formEmail.addEventListener('focus', function () {
+    document
+        .getElementById('email')
+        .style
+        .backgroundColor = "#EEAD0E";
 });
 
-formEmail.addEventListener('blur',function() {
-    document.getElementById('email').style.backgroundColor = "#CFCFCF";
+formEmail.addEventListener('blur', function () {
+    document
+        .getElementById('email')
+        .style
+        .backgroundColor = "#CFCFCF";
 });
 
-var buyButtonVeneza = document.querySelector('#checkDaysInVeneza').addEventListener('submit', function(e) {
-    let daysVeneza = document.forms[1].numDays1.value;
-    e.preventDefault();
-    if ((checkUsuario() == true) && (checkDay1 == true)){
-        travels.push( new Travel("Veneza",daysVeneza));
-        alert('Compra efetivada!');
-    }
-});
+var buyButtonVeneza = document
+    .querySelector('#checkDaysInVeneza')
+    .addEventListener('submit', function (e) {
+        let daysVeneza = document
+            .forms[1]
+            .numDays1
+            .value;
+        e.preventDefault();
+        if ((checkUsuario() == true) && (checkDay1 == true)) {
+            travels.push(new Travel("Veneza", daysVeneza));
+            alert('Compra efetivada!');
+        }
+    });
 
-var buyButtonVeneza = document.querySelector('#checkDaysInLondon').addEventListener('submit', function(e) {
-    let daysLondon = document.forms[2].numDays2.value;
-    e.preventDefault();
-    if ((checkUsuario() == true) && (checkDay1 == true)){
-        travels.push( new Travel("Londres",daysLondon));
-        alert('Compra efetivada!');
-    }
-});
+var buyButtonLondon = document
+    .querySelector('#checkDaysInLondon')
+    .addEventListener('submit', function (e) {
+        let daysLondon = document
+            .forms[2]
+            .numDays2
+            .value;
+        e.preventDefault();
+        if ((checkUsuario() == true) && (checkDay1 == true)) {
+            travels.push(new Travel("Londres", daysLondon));
+            alert('Compra efetivada!');
+        }
+    });
 
 var travels = [];
 
 let screen = window;
-screen.onload = function() {
+screen.onload = function () {
     alert('Bem-vindo ao nosso site de Vendas😃');
 };
 
 let telefone;
-(function() {
-    setTimeout(function() {
-        telefone = prompt('Gostaria de nos dizer seu telefone?'); 
+(function () {
+    setTimeout(function () {
+        telefone = prompt('Gostaria de nos dizer seu telefone?');
     }, 3000);
 })();
 
-var checkNumberFunction = function() {
-    if (telefone != null){
+var checkNumberFunction = function () {
+    if (telefone != null) {
         return true;
-    }else{
+    } else {
         return false;
     }
 };
 
-document.querySelector('#h2-create-user').addEventListener('mouseover', function() {
-    alert('Crie um Usuario para efetuar compras!');
-});
+document
+    .querySelector('#h2-create-user')
+    .addEventListener('mouseover', function () {
+        alert('Crie um Usuario para efetuar compras!');
+    });
 
 //Controle de compra de viagens
-var checkUsuario = function() {
-    if (checkIfIsAnyUserIsCreated() == false){
+var checkUsuario = function () {
+    if (checkIfIsAnyUserIsCreated() == false) {
         window.alert('AVISO:É necessário criar um usuário para efetivar a compra!');
         return false;
-    }else if( checkIfIsAnyUserIsCreated() == true){
+    } else if (checkIfIsAnyUserIsCreated() == true) {
         return true;
     }
 };
 
-var checkIfIsAnyUserIsCreated = function() {
-    if (users.length == 0){
+var checkIfIsAnyUserIsCreated = function () {
+    if (users.length == 0) {
         return false;
-    }else{
+    } else {
         return true;
     }
+};
+
+document
+    .querySelector('body')
+    .addEventListener('keydown', function () {
+        let key = event.keyCode;
+        if (key === 27) {
+            getTravels();
+        }
+    });
+
+function getTravels() {
+    travels.forEach(e => {
+        alert('Destino:' + e.destino + ', ' + e.dias + ' dias');
+    });
+}
+
+(function () {
+    document
+        .forms[4]
+        .elements[0]
+        .value = 'Deixe seu comentário!';
+}());
+
+document
+    .querySelector('#comentSpace')
+    .addEventListener('focus', function () {
+        window.document.forms.commentSection.comentSpace.value = '';
+    });
+
+document
+    .querySelector('#comentSpace')
+    .addEventListener('blur', function () {
+        window.document.forms.commentSection.comentSpace.value = 'Deixe seu comentário!';
+    });
+
+let h2CreateUserMessage = document.querySelector('#h2-create-user');
+
+document
+    .querySelector('.sign-in')
+    .addEventListener('mouseover', function () {
+        h2CreateUserMessage.innerHTML = 'Criando Usuário...';
+    });
+
+document
+    .querySelector('.sign-in')
+    .addEventListener('mouseout', function () {
+        h2CreateUserMessage.innerHTML = 'Crie seu Usuário:';
+    });
+
+window.onload = function () {
+    let inputCamp = document.querySelector('#numDays3');
+
+    inputCamp.addEventListener('blur', function () {
+        if (this.validity.valueMissing) {
+            this.setCustomValidity('AVISO: Preencha esse campo!');
+        }
+    });
+
+    inputCamp.addEventListener('blur', function () {
+        if (this.validity.rangeOverFlow) {
+            this.setCustomValidity(
+                'AVISO: Somente podem ser compradas viagens de até 10 dias!'
+            );
+        }
+    });
+
+    inputCamp.addEventListener('blur', function () {
+        if (this.validity.rangeUnderflow) {
+            this.setCustomValidity('AVISO: O mínimo de dias para uma viagem é 1 dia!');
+        }
+    });
+
+    inputCamp.addEventListener('blur', function () {
+        if (checkIfIsAnyUserIsCreated() === false) {
+            alert('AVISO:É necessário criar um usuário para efetivar a compra!');
+        }
+    });
+
+    inputCamp.addEventListener('blur', function () {
+        if (this.validity.valid) {
+            let daysValue = document
+                .forms[3]
+                .numDays3
+                .value;
+            travels.push(new Travel("Egito", daysValue));
+        }
+    });
 };
 
 let coments = [];
@@ -169,49 +273,17 @@ function postComentTradePage(comentSpace) {
     alert('Muito obrigado por deixar um comentário!');
 }
 
-document.querySelector('body').addEventListener('keydown', function() {
-    let key = event.keyCode;
-    if (key === 27){
-        getTravels();
-    }
-});
-
-function getTravels() {
-    travels.forEach(e =>{
-        alert('Destino:'+ e.destino + ', ' + e.dias + ' dias');
-    });
-}
-
-window.onload = function() {
-    document.forms[1].elements[0].value = 'Deixe seu comentário!';
-};
-
-document.querySelector('#comentSpace').addEventListener('focus', function() {
-    window.document.forms.commentSection.comentSpace.value = '';
-});
-
-document.querySelector('#comentSpace').addEventListener('blur', function() {
-    window.document.forms.commentSection.comentSpace.value = 'Deixe seu comentário!';
-});
- 
-let h2CreateUserMessage = document.querySelector('#h2-create-user');
-
- document.querySelector('.sign-in').addEventListener('mouseover', function() {
-    h2CreateUserMessage.innerHTML = 'Criando Usuário...';
-});
-
-document.querySelector('.sign-in').addEventListener('mouseout', function() {
-    h2CreateUserMessage.innerHTML = 'Crie seu Usuário:';
-});
-
 // Função de Validação Modo Tradicional
 let checkDay1 = false;
 function checkDays1() {
-    let daysValue = document.forms[1].numDays1.value;
-    if ((daysValue < 0) || (daysValue > 10) || (daysValue == null) || (daysValue == "")){
+    let daysValue = document
+        .forms[1]
+        .numDays1
+        .value;
+    if ((daysValue < 0) || (daysValue > 10) || (daysValue == null) || (daysValue == "")) {
         checkDay1 = false;
         return false;
-    }else{
+    } else {
         checkDay1 = true;
         return true;
     }
@@ -219,11 +291,14 @@ function checkDays1() {
 
 let checkDay2 = false;
 function checkDays2() {
-    let daysValue = document.forms[2].numDays2.value;
-    if ((daysValue < 0) || (daysValue > 10) || (daysValue == null) || (daysValue == "")){
+    let daysValue = document
+        .forms[2]
+        .numDays2
+        .value;
+    if ((daysValue < 0) || (daysValue > 10) || (daysValue == null) || (daysValue == "")) {
         checkDay1 = false;
         return false;
-    }else{
+    } else {
         checkDay1 = true;
         return true;
     }
@@ -231,44 +306,9 @@ function checkDays2() {
 
 // Validação usando API HTML 5
 function checkDays3() {
-    if(checkIfIsAnyUserIsCreated() === true){
+    if (checkIfIsAnyUserIsCreated() === true) {
         alert('Compra efetivada!');
-    }else{
+    } else {
         alert('AVISO:É necessário criar um usuário para efetivar a compra!');
     }
 }
-
-window.onload = function() {
-    let inputCamp = document.querySelector('#numDays3');
-
-    inputCamp.addEventListener('blur', function(){
-        if (this.validity.valueMissing){
-            this.setCustomValidity('AVISO: Preencha esse campo!');
-        }
-    });
-
-    inputCamp.addEventListener('blur', function(){
-        if (!this.validity.rangeOverFlow){
-            this.setCustomValidity('AVISO: Somente podem ser compradas viagens de até 10 dias!');
-        }
-    });
-
-    inputCamp.addEventListener('blur' ,function(){
-        if (this.validity.rangeUnderflow){
-            this.setCustomValidity('AVISO: O mínimo de dias para uma viagem é 1 dia!');
-        }
-    });
-
-    inputCamp.addEventListener('blur', function(){
-        if(checkIfIsAnyUserIsCreated() === true){
-            alert('AVISO:É necessário criar um usuário para efetivar a compra!');
-        }
-    });
-
-    inputCamp.addEventListener('blur', function(){
-        if (this.validity.valid){
-            let daysValue = document.forms[3].numDays3.value;
-            travels.push(new Travel("Egito", daysValue));
-        }
-    });
-};
